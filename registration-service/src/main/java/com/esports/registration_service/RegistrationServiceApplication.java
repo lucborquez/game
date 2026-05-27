7e3a371ba2e4efff1a0805f316e9aa95e911e71e
Lucas <luc.borquez@duocuc.cl>
2026-05-27 02:21:32 -0400

    Configuración inicial del microservicio registation-service
    - Establecer nombre de aplicación como registration-service
    - Definir puerto 8086 para evitar conflictos
    - Configurar conexión a base de datos MySQL (db_registration_service)
    - Habilitar configuración JPA/Hibernate para actualización automática de esquema

--- registration-service/src/main/java/com/esports/registration_service/RegistrationServiceApplication.java
+++ registration-service/src/main/java/com/esports/registration_service/RegistrationServiceApplication.java
@@ -1,4 +1,4 @@
-package com.esports.manager.registration;
+package com.esports.registration_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RegistrationServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RegistrationServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationServiceApplication.class, args);
+	}
 
 }
--- registration-service/src/test/java/com/esports/registration_service/RegistrationServiceApplicationTests.java
+++ registration-service/src/test/java/com/esports/registration_service/RegistrationServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.esports.manager.registration;
+package com.esports.registration_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RegistrationServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
